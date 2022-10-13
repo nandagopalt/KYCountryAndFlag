a8f69a65d1204c949986be3d03b425bec032a1b3
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-10-13 20:14:48 +0530

    Including the depedendencies for third party libraries and code for connecting with remote service through RxJava,RxAndroid and Retrofit

--- app/src/main/java/com/amalwin/kycountryandflag/models/Country.kt
+++ app/src/main/java/com/amalwin/kycountryandflag/models/Country.kt
@@ -1,3 +1,16 @@
 package com.amalwin.kycountryandflag.models
 
-data class Country(var name:String)
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Country(
+    @SerializedName("name")
+    @Expose
+    val countryName: String?,
+    @SerializedName("capital")
+    @Expose
+    val capitalName: String?,
+    @SerializedName("flagPNG")
+    @Expose
+    val countryFlag: String?
+)
